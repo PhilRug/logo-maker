15c0ad499979de1263c11c478d6e6b084897b109
PhilRug <ruggiero.pj@gmail.com>
2023-04-01 18:10:57 -0400

    added shape classes and prompts

--- README.md
+++ README.md
@@ -12,16 +12,20 @@ With this app, you can create a simple custom  logo by answering a few questions
 - [Credits](#credits)
     
 ## Installation
-npm node v 16 
+1. npm node v 16 
+2. jest: npm install --save-dev jest
+3. inquirer: npm install --save inquirer
+
     
 ## Usage
-all you need to do it answer the questions prompted by the terminal
+all you need to do it answer the questions prompted by your integrated terminal. you can start by right clicking the index.js file and select "open in integrated terminal", then type "node index.js".
     
 ## Testing
 testing is done with jest
 
 ## Credits
-n/a
+- shapes code: https://www.w3.org/TR/SVG2/shapes.html
+- triangle dimensions: https://www.w3schools.com/graphics/tryit.asp?filename=trysvg_polygon
     
 ## Questions
 If you have any questions you can contact me at:
--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Logo = require('./lib/shapes');
 
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What 3 characters would you like to use for your logo?',
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'What color would you like your text?',
+        name: 'textcolor',
+    },
+    {
+        type: 'list',
+        message: 'What shape would you like to use',
+        name: 'shape',
+        choices: ['Square', 'Circle', 'Triangle'],
+    },
+    {
+        type: 'input',
+        message: 'What color would you like your shape to be?',
+        name: 'shapecolor',
+    }
+]).then(response => {
+    console.log(response);
+    writeToFile(response);
+})
+
+function writeToFile({text, textcolor, shape, shapecolor}) {
+    const logo = 
+`
+`
+
+
+
+fs.writeFile('logo.svg', logo, (err) => err ? console.error(error) : console.log("Success!"));
+}
